bea34a561c1c0db1f067401bb179726a5b440932
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2020-11-15 23:58:36 -0800

    Something for now. Will update more later.

--- backend/search_engine.py
+++ backend/search_engine.py
@@ -2,14 +2,26 @@ import elasticsearch
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-es = Elasticsearch()
-URI = "mongodb+srv://user:OkfGMkxHXu1FouKu@cluster0.jtm8k.mongodb.net/proconnect?retryWrites=true&w=majority"
-client = MongoClient(URI)
+def mongo_search(mongo_search_function):
+    pass
 
-# FOR TESTING:
-##
+def main(argv):
 
-try:
-    client.admin.command()
-except ConnectionFailure:
-    print("Server not available")
+    es = Elasticsearch()
+    URI = "mongodb+srv://user:OkfGMkxHXu1FouKu@cluster0.jtm8k.mongodb.net/proconnect?retryWrites=true&w=majority"
+    client = MongoClient(URI)
+
+    # FOR TESTING:
+    ##
+
+    client.
+
+
+
+if __name__ == "__main__":
+
+    try:
+        client.admin.command()
+        main(sys.argv[1:])
+    except ConnectionFailure:
+        print("Server not available")

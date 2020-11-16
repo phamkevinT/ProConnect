538c56a728ad909f4a8e6ee9435f9d5e0bd084c4
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2020-11-16 01:28:42 -0800

    Made some more changes. More to come.

--- backend/search_engine.py
+++ backend/search_engine.py
@@ -1,5 +1,7 @@
-import elasticsearch
-from pymongo import MongoClient
+import sys, getopt, time
+
+from Elasticsearch import elasticsearch
+from pymongo import MongoClient, uri_parser
 from pymongo.errors import ConnectionFailure
 
 def mongo_search(mongo_search_function):
@@ -7,21 +9,24 @@ def mongo_search(mongo_search_function):
 
 def main(argv):
 
-    es = Elasticsearch()
-    URI = "mongodb+srv://user:OkfGMkxHXu1FouKu@cluster0.jtm8k.mongodb.net/proconnect?retryWrites=true&w=majority"
-    client = MongoClient(URI)
+    es = ElasticSearch()
+    uri = argv[2]
 
-    # FOR TESTING:
-    ##
+    try:
+        client = MongoClient(uri)
 
-    client.
+    except uri_parser.parser_uri(uri, warn=False):
+        print("Invalid MongoDB URI")
+        sys.exit(2)
 
+    except getopt.GetoptError:
+        print('usage: search_engine.py <argument 1, argument 2...>')
+        sys.exit(2)
 
 
 if __name__ == "__main__":
 
     try:
-        client.admin.command()
         main(sys.argv[1:])
     except ConnectionFailure:
         print("Server not available")
